8c8520ab67c3733734ee52ef26b3bbebf1dfc83c
Miguel Capilla Miralles <1cecapillamiguel@gmail.com>
2019-11-29 10:14:27 +0100

    Primer Commit!

---
+++ src/obtenerLibros/obtenerLibros.java
@@ -0,0 +1,10 @@
+package obtenerLibros;
+
+public class obtenerLibros {
+
+	public static void main(String[] args) {
+		Libro libro1 = new Libro("El nombre del viento", "Patrick Rothfuss", 1200);
+		System.out.printf("%-40s%-40s%-4d, args)
+	}
+
+}
